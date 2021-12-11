37090dc4aabfe68ddcb6f9a89c341400e2e49283
gourabk121 <44121563+gourabk121@users.noreply.github.com>
2021-12-11 19:12:10 +0530

    Create ReverseArray.java

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + " ");
+    }
+    System.out.println(sb);
+  }
+
+  public static void reverse(int[] a){
+      int si=0;
+      int li=a.length-1;
+     while(li>si){
+          int temp=a[li];
+          a[li]=a[si];
+          a[si]=temp;
+          li--;
+          si++;
+          
+      }
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    reverse(a);
+    display(a);
+ }
+
+}
